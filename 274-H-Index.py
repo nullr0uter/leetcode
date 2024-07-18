ba66cca0249aa68b087e43872cc0e6b41417bc01
Lukas <47256687+nullr0uter@users.noreply.github.com>
2024-07-18 08:46:56 +0200

    Create 274-H-Index.py

---
+++ 274-H-Index.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        for i, v in enumerate(citations):
+            if n - i <= v:
+                return n - i
+        return 0
