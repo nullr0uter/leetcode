6541c4ce433743a37df832fc581617654d92da4f
Lukas <47256687+nullr0uter@users.noreply.github.com>
2024-11-29 18:34:06 +0100

    Update [C#]-TwoSum.cs

--- [C#]-TwoSum.cs
+++ [C#]-TwoSum.cs
@@ -4,37 +4,74 @@
 
 using System;
 
-public class Program
+namespace TwoSumCS
 {
-  public static void Main(string[] args)
-  {
-    Program p = new Program();
-    int[] result = p.twoSum(new int[] {2, 7, 11, 15}, 9);
-    Console.WriteLine($"[{result[0]}, {result[1]}]");
-  }
-
-  public int[] twoSum(int[] nums, int target)
-  {
-    Array.Sort(nums);
-    int[] result = new int[2];
-    int left = 0;
-    int right = nums.Length - 1;
-    while (left < right)
+    class Program
     {
-      int tmp = nums[left] + nums[right];
-      if (tmp == target) {
-        result[0] = left;
-        result[1] = right;
-        break;
-      } else if (tmp < target) {
-        left++;
-      } else if (tmp > target) {
-        --right;
-      } else {
-        int[] noResult = {};
-        return noResult;
-      }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] nums = new int[5] { 2, 3, 5, 9, 4 };
+            int target = 6;
+            int[] result = new int[2];
+            result = program.twoSum(nums, target);
+            program.printResult(nums, target, result);
+        }
+        private int[] twoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            Array.Sort(nums);
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left < right)
+            {
+                int tmp = nums[left] + nums[right];
+                if (tmp == target)
+                {
+                    result[0] = left + 1;
+                    result[1] = right + 1;
+                    break;
+                }
+                else if (tmp < target)
+                {
+                    left++;
+                }
+                else if (tmp > target)
+                {
+                    right--;
+                }
+                else
+                {
+                    int[] none = new int[0];
+                    return none;
+                }
+            }
+            return result;
+        }
+        private void printResult(int[] nums, int target, int[] result)
+        {
+            Array.Sort(nums);
+            Console.Write("Sorted: {");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i]);
+                if (i != nums.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.WriteLine("}");
+            Console.WriteLine("Target: {0}", target);
+            Console.Write("Result: {");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i]);
+                if (i != result.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.WriteLine("}");
+        }
     }
-    return result;
-  }
 }
